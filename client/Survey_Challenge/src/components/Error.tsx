f4b535493dc01332f9e9e7587e9b7e313f6f7edf
Davidoar15 <davidoar15@gmail.com>
2023-09-05 12:54:10 -0300

    add/initial_landing&error

--- client/Survey_Challenge/src/components/Error.tsx
+++ client/Survey_Challenge/src/components/Error.tsx
@@ -1,10 +1,18 @@
-
+import errorImage from '../assets/Survey-error-image.png';
 
 function Error() {
 
   return (
     <div>
-        <h1>ERROR</h1>
+        <div>
+          <img src={errorImage} alt='Page Error'/>
+        </div>
+        <h1>ERROR 404: Página No Encontrada</h1>
+        <h3>
+          Vaya, parece que no podemos encontrar 
+          la página que buscas. Intenta volver a 
+          la página anterior
+        </h3>
     </div>
   );
 }
--- client/Survey_Challenge/src/components/Landing.tsx
+++ client/Survey_Challenge/src/components/Landing.tsx
@@ -1,8 +1,18 @@
+import { NavLink } from "react-router-dom";
+
 function Landing() {
 
     return (
       <div>
           <h1>WELCOME</h1>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, exercitationem labore iusto molestiae omnis, perspiciatis minima sunt inventore reprehenderit dolores asperiores? Voluptas excepturi soluta quidem dolorem et cupiditate error eaque.
+            </p>
+          </div>
+          <NavLink to={"/survey"}>
+            <button>Start</button>
+          </NavLink>
       </div>
     );
   }
